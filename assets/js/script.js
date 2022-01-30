82d7a2696e5316937c93685e12863bc55e4593ca
Leigh Pfeiffer <leigh.pfeiffer@gmail.com>
2022-01-30 16:59:26 -0500

    funcitionality and intro css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,74 +1,149 @@
+
+
 // global variables
 var currentTime = 100
-var questionIndex = 0
+var questionIndex = -1
 var timerInterval;
+var finalScore;
+if(!localStorage.getItem("Highscores")){
+    var highScores = []
+}else {
+    var highScores = JSON.parse(localStorage.getItem("Highscores"))
+}
 // Selectors
 var timerNumber = document.querySelector("#timer-number")
 var startButton = document.querySelector("#start-button")
+var startDiv = document.querySelector("#start-div")
 var questionSection = document.querySelector("#question-section")
 var answerSection = document.querySelector("#answer-section")
 var responceSection = document.querySelector("#responce-section")
 var scoreSection = document.querySelector("#score-section")
 var quizSection = document.querySelector("#quiz-section")
 var restartButton = document.querySelector("#restart")
+var submissionSection = document.querySelector("#submission-section")
+var score = document.querySelector("#score")
+var initals = document.querySelector("#initals")
+var submitButton = document.querySelector("#submit")
+var scoreList = document.querySelector("#score-list")
+var quizIntro = document.querySelector("#quiz-intro")
 
 //Questions
 var questionArray = [{
     question: "What is an array?",
-    answers: ["Set of properties", "Single varrible used to store elements", "A single value"]
+    answers: ["Set of properties", "Single varrible used to store elements", "A single value"],
+    correctAnswer: 1
 },
 {
     question: "Which is an assignment operator?",
-    answers: ["=", "==", "==="]
+    answers: ["=", "==", "==="],
+    correctAnswer: 0
 },
 {
     question: "What key word allows you to leave a function early?",
-    answers: ["stop", "end", "break"]
+    answers: ["stop", "end", "break"],
+    correctAnswer: 2
 }
 ];
 
-function renderNextQuestion() {
+//move to next question
+function renderNextQuestion(event) {
+    if(questionIndex !== -1){
+        checkAnswer(event.target.id)
+    }
+    questionIndex++
     if(questionIndex < questionArray.length){
     questionSection.innerHTML = questionArray[questionIndex].question;
     answerSection.innerHTML = ""
     for (var i = 0; i < questionArray[questionIndex].answers.length; i++){
         var button = document.createElement("button")
-        button.addEventListener("click", renderNextQuestion)
+        button.setAttribute("id", i)
+        button.setAttribute("class", "answer-button")
+        button.addEventListener("click", function(e){
+            event.preventDefault
+            renderNextQuestion(e);
+        })
         button.innerHTML = questionArray[questionIndex].answers[i]
         answerSection.appendChild(button)
     };
-    questionIndex++
     }else{
         endGame()
     }
 }
+
+// Check answer
+function checkAnswer(answer) {
+    console.log(answer)
+    if(answer == questionArray[questionIndex].correctAnswer){
+        responceSection.innerHTML = "Last Question Was Correct"
+        setTimeout( function(){responceSection.innerHTML = ""}, 2000)
+        currentTime = currentTime + 10
+    }else{
+        responceSection.innerHTML = "Last Question Was Wrong"
+        setTimeout( function(){responceSection.innerHTML = ""}, 2000)
+        currentTime = currentTime - 10
+    }
+}
+
 // Start Quiz
-function startQuiz() {
-    startButton.setAttribute("hidden", "true")
-    renderNextQuestion()
+function startQuiz(event) {
+    event.preventDefault
+    startDiv.setAttribute("hidden", "true")
+    quizIntro.setAttribute("hidden", "true")
+    renderNextQuestion(event)
     timerInterval = setInterval(function () {
-        currentTime--
-        timerNumber.innerHTML = currentTime
+        if(currentTime > 0){
+            currentTime--
+            timerNumber.innerHTML = currentTime
+            
+        }else{
+            endGame()
+        }
     }, 1000)
+    
 }
 
 
 //end game
 function endGame() {
     quizSection.setAttribute("hidden", "true")
-    scoreSection.removeAttribute("hidden")
-    startButton.removeAttribute("hidden")
+    submissionSection.removeAttribute("hidden")
+    startDiv.removeAttribute("hidden")
     answerSection.innerHTML = ""
     questionSection.innerHTML = ""
+    finalScore = currentTime
+    score.innerHTML = finalScore
     clearInterval(timerInterval)
-    currentTime = 100
     questionIndex = 0
+}
+
+//Submitting Highscores
+function submit(){
+    highScores.push(initals.value + " - " + finalScore)
+    localStorage.setItem("Highscores", JSON.stringify(highScores))
+    renderHighscores()
+}
+
+//render highscores
+function renderHighscores(){
+    scoreList.innerHTML = ""
+    for(var i = 0; i < highScores.length; i++){
+        var item = document.createElement("li")
+        item.innerHTML = highScores[i]
+        scoreList.appendChild(item)
+        
+    }
+    submissionSection.setAttribute("hidden", "true")
+    scoreSection.removeAttribute("hidden")
+    quizSection.setAttribute("hidden", "true")
 }
 
 //restart
 function restart() {
     scoreSection.setAttribute("hidden", "true")
     quizSection.removeAttribute("hidden")
+    currentTime = 100
+    timerNumber.innerHTML = currentTime
 }
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", function(event){startQuiz(event)})
 restartButton.addEventListener("click", restart)
+submitButton.addEventListener("click", submit)
